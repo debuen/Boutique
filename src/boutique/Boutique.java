0c062f67022d9e56772086eae8be3baec6819977
DAW <DAW@localhost>
2017-11-07 10:54:43 +0100

    EN PROCESO

--- src/boutique/Boutique.java
+++ src/boutique/Boutique.java
@@ -6,7 +6,11 @@ public class Boutique {
 
     
     public static void main(String[] args) {
-        
+        Menu principal = new Menu();
+        // Centramos la ventana en la pantalla
+        principal.setLocationRelativeTo(null);
+        // Lo hacemos visible
+        principal.setVisible(true);
     }
     
 }
